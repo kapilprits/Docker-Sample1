2624f5f8ff43718d5dc0fea94e5883592d07e52a
Kapil Singi <Kapil_s@techlabs.accenture.com>
2016-12-16 16:17:04 +0530

    Add all my files

---
+++ sample/src/main/java/com/accenture/sample/App.java
@@ -0,0 +1,16 @@
+package com.accenture.sample;
+
+import com.google.gson.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!"));
+    }
+}
